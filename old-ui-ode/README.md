2d4bc10eef2bdce8967578e41f0056b5fd13ab54
WHNLS <mbaraka.itcomp@gmail.com>
2026-07-12 16:56:58 +0100

    Update : - Remove .env from tracking and add to gitignore - Removed old-ui-code directory as its not needed anymore - added local supabase support !

--- old-ui-ode/README.md
+++
@@ -1,3 +0,0 @@
-# YOUENGAGE-DEV
-
-[![Open in Bolt](https://bolt.new/static/open-in-bolt.svg)](https://bolt.new/~/sb1-myqu3ep4)
--- old-ui-ode/src/features/phase2-ui/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './adapters';
-export * from './components';
-export * from './player';
-export * from './theme';
-export * from './types';
